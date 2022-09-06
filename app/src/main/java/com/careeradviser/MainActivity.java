73ed655620a05c86e2c4b252d46ea02b4536e0b1
Ivaan2 <ivan.m.m.2002.ivi@gmail.com>
2022-09-06 14:43:33 +0200

    Boton definido y declarado

--- app/src/main/java/com/careeradviser/MainActivity.java
+++ app/src/main/java/com/careeradviser/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.EventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addCareerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addCareerBtn = this.findViewById(R.id.addCareerBtn);
+        //addCareerBtn.setOnClickListener();
     }
 }
